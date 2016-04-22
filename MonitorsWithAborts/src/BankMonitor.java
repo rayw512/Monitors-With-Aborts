9dfed9579f8bf8948f27f78673ebe9699ee3c144
Raymond Wen <raymond.wen@utexas.edu>
2016-04-22 12:44:28 -0500

    formatting

--- MonitorsWithAborts/src/BankMonitor.java
+++ MonitorsWithAborts/src/BankMonitor.java
@@ -10,26 +10,26 @@ class BankMonitor extends MonitorsWithAborts implements Serializable {
 
 	public synchronized void deposit(int amount) throws InterruptedException, IOException, ClassNotFoundException {
 		this.savestate();
-		System.out.println("Balance saved: "+ balance);
-		balance=balance+amount;
-		System.out.println("Balance before abort: "+ balance );
-		BankMonitor saved=(BankMonitor)this.abort();
-		balance=saved.getBalance();
-		System.out.println("Balance after abort: "+ balance );
-		
+		System.out.println("Balance saved: " + balance);
+		balance = balance + amount;
+		System.out.println("Balance before abort: " + balance);
+		BankMonitor saved = (BankMonitor) this.abort();
+		balance = saved.getBalance();
+		System.out.println("Balance after abort: " + balance);
+
 	}
 
 	public synchronized void withdraw(int amount) throws InterruptedException {
-		
-		balance=balance-amount;
-		
+
+		balance = balance - amount;
+
 	}
-	public synchronized int getBalance()
-	{
+
+	public synchronized int getBalance() {
 		return balance;
 	}
-	public synchronized void setBalance(int newBalance)
-	{
-		balance=newBalance;
+
+	public synchronized void setBalance(int newBalance) {
+		balance = newBalance;
 	}
 }
--- MonitorsWithAborts/src/MonitorsWithAborts.java
+++ MonitorsWithAborts/src/MonitorsWithAborts.java
@@ -1,28 +1,27 @@
 import java.io.*;
 
 public class MonitorsWithAborts {
-	public void savestate() throws IOException
-	{
-        FileOutputStream fileOut =
-        new FileOutputStream("savedState.ser");
-        ObjectOutputStream out = new ObjectOutputStream(fileOut);
-        out.writeObject(this);
-        out.close();
-        fileOut.close();
+	public void savestate() throws IOException {
+		FileOutputStream fileOut = new FileOutputStream("savedState.ser");
+		ObjectOutputStream out = new ObjectOutputStream(fileOut);
+		out.writeObject(this);
+		out.close();
+		fileOut.close();
 	}
+
 	/**
 	 * 
-	 * @param <T> the type of the value
-	 * @throws IOException 
-	 * @throws ClassNotFoundException 
+	 * @param <T>
+	 *            the type of the value
+	 * @throws IOException
+	 * @throws ClassNotFoundException
 	 */
-	public Object abort() throws IOException, ClassNotFoundException
-	{
-        FileInputStream fileIn = new FileInputStream("savedState.ser");
-        ObjectInputStream in = new ObjectInputStream(fileIn);
-        Object obj =in.readObject();
-        in.close();
-        fileIn.close();
-        return obj;
+	public Object abort() throws IOException, ClassNotFoundException {
+		FileInputStream fileIn = new FileInputStream("savedState.ser");
+		ObjectInputStream in = new ObjectInputStream(fileIn);
+		Object obj = in.readObject();
+		in.close();
+		fileIn.close();
+		return obj;
 	}
 }
